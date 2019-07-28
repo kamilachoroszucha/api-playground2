f4b96416804763a376cab179db0771628f7601c3
Kamila Choroszucha <kamila.choroszucha@gmail.com>
2019-07-28 11:57:46 +0200

    feat: content and styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,42 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+fetch("https://api.openweathermap.org/data/2.5/group?id=763166,756135,3099434,776069,3081368,3096472,3083829,3088171,3080866,2950159,3143244,2643743,683506,3413829&APPID=d9b4d047a1d04c2aab0936d7fd2a6b64&units=metric")
+.then(resp => resp.json())
+.then(resp => {
+  console.log(resp);
+  const temp=[];
+  const cities=[];
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+  console.log(resp.list);//array z 2 elementami
+ 
+  for (let i =0;i<(resp.list).length; i++){
+    temp.push(Math.round(resp.list[i].main.temp))};
+  console.log(typeof(temp[0]));
+  console.log(temp);
 
-// place your code below
+  for (let i =0;i<(resp.list).length; i++){
+    cities.push(resp.list[i].name)};
+  console.log(cities);
+  const text = document.querySelector('.form__temp--js');
+  console.log(text.innerHTML);
+  text.addEventListener('keyup', (e)=>{
+    
+    console.log((e.target.value));
+    const result = parseInt(e.target.value);//zmiana na liczbę
+    console.log(result);
+    const index = temp.indexOf(result);
+    console.log(index);
+    const city = cities[index];
+    console.log(cities[index]);
+    const anwser = document.querySelector('.answer--js');
+    
+    if(index>=0){
+      anwser.innerHTML = `You should go to ${city}! `
+    }else{
+      anwser.innerHTML = `Stop whining! There is no place with this temperature anywhere nearby! Be tough!`
+    }
+  })
+})
 
-
-console.log(`Hello world!`);
 
 
